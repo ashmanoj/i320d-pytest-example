1a692e21785faa1fb6005067462ec73b3be192ea
Aswathi Manoj <am98247@educcomp02.ccbb.utexas.edu>
2025-03-26 16:31:34 -0500

    two tests will fail

--- my_code.py
+++ my_code.py
@@ -2,6 +2,7 @@ import pytest
 
 def fix_phone_num(phone_num_to_fix):
   # given "5125558823". Split the parts, then recombine and return
+
   area_code = phone_num_to_fix[0:3] # 512 (first three digits)
   three_part = phone_num_to_fix[3:6] # 555 (next three digits)
   four_part = phone_num_to_fix[6:] # # 8823 (last four digits)
@@ -12,3 +13,21 @@ def fix_phone_num(phone_num_to_fix):
 
 def test_fix_phone_num():
   assert fix_phone_num("5125558823") == '(512) 555 8823'
+  
+
+  
+def test_fix_phone_num2():
+  assert fix_phone_num("5554429876") == '(555) 442 9876'
+  
+def test_fix_phone_num3():
+  assert fix_phone_num("3216543333") == '(321) 654 3333'
+  
+def test_fix_phone_num_dashes():
+  with pytest.raises(ValueError):
+    fix_phone_num("555-442-98761")
+
+def test_fix_phone_num_formatted():
+  with pytest.raises(ValueError):
+    fix_phone_num("(321) 654 3333")
+
+

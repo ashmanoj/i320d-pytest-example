a5dfe3f1bcb5a93523b35a56a8cd8a483bd7c993
Aswathi Manoj <am98247@educcomp02.ccbb.utexas.edu>
2025-03-26 16:42:02 -0500

    tests that are not digits are not raising value error

--- my_code.py
+++ my_code.py
@@ -3,6 +3,7 @@ import pytest
 def fix_phone_num(phone_num_to_fix):
   # given "5125558823". Split the parts, then recombine and return
 
+  
   area_code = phone_num_to_fix[0:3] # 512 (first three digits)
   three_part = phone_num_to_fix[3:6] # 555 (next three digits)
   four_part = phone_num_to_fix[6:] # # 8823 (last four digits)
@@ -29,5 +30,10 @@ def test_fix_phone_num_dashes():
 def test_fix_phone_num_formatted():
   with pytest.raises(ValueError):
     fix_phone_num("(321) 654 3333")
+
+def test_fix_phone_num_is_digits():
+  with pytest.raises(ValueError):
+    phone_num_to_fix("abcd")
+
 
 
